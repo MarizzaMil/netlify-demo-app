61a5dc9ab144bea526edd5901d52c21a04dca52c
MarizzaMil <marizza.mil89@gmail.com>
2023-08-28 09:48:05 +0300

    fix

--- netlify/functions/heroes/router.js
+++ netlify/functions/heroes/router.js
@@ -3,68 +3,69 @@ const getHeroesModel = require('./model');
 const router = express.Router()
 
 router.get('/heroes', async (req, res) => {
-  const HeroModel = getHeroesModel();
+    console.log("GET")
+    const HeroModel = getHeroesModel();
 
-  HeroModel.findAll((heroes) => {
-      res.status(200).json(heroes);
-  });
+    HeroModel.findAll((heroes) => {
+        res.status(200).json(heroes);
+    });
 });
 
 
 router.post('/heroes', async (req, res) => {
-  const { name, superPower } = req.body;
+    const { name, superPower } = req.body;
 
-  if (!name || !superPower) {
-    return res.status(400).json({ message: 'Name and superPower are required' });
-  }
-
-  const HeroModel = getHeroesModel(); // Initialize the model
-
-  HeroModel.create(name, superPower, (err, message) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).json({ message: 'Error creating hero' });
+    if (!name || !superPower) {
+        return res.status(400).json({ message: 'Name and superPower are required' });
     }
 
-    return res.status(201).json({ message: 'Hero created successfully' });
-  });
+    const HeroModel = getHeroesModel(); // Initialize the model
+
+    HeroModel.create(name, superPower, (err, message) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({ message: 'Error creating hero' });
+        }
+
+        return res.status(201).json({ message: 'Hero created successfully' });
+    });
 });
 
 // Update a hero by ID
 router.put('/heroes/:id', async (req, res) => {
-  const { id } = req.params;
-  const { name, superPower } = req.body;
+    const { id } = req.params;
+    const { name, superPower } = req.body;
 
-  if (!name || !superPower) {
-    return res.status(400).json({ message: 'Name and superPower are required' });
-  }
-
-  const HeroModel = getHeroesModel(); // Initialize the model
-
-  HeroModel.update(id, name, superPower, (err, message) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).json({ message: 'Error updating hero' });
+    if (!name || !superPower) {
+        return res.status(400).json({ message: 'Name and superPower are required' });
     }
 
-    return res.status(200).json({ message: 'Hero updated successfully' });
-  });
+    const HeroModel = getHeroesModel(); // Initialize the model
+
+    HeroModel.update(id, name, superPower, (err, message) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({ message: 'Error updating hero' });
+        }
+
+        return res.status(200).json({ message: 'Hero updated successfully' });
+    });
 });
 
 // Delete a hero by ID
 router.delete('/heroes/:id', async (req, res) => {
-  const { id } = req.params;
+    const { id } = req.params;
 
-  const HeroModel = getHeroesModel(); // Initialize the model
+    const HeroModel = getHeroesModel(); // Initialize the model
 
-  HeroModel.delete(id, (err, message) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).json({ message: 'Error deleting hero' });
-    }
+    HeroModel.delete(id, (err, message) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({ message: 'Error deleting hero' });
+        }
 
-    return res.status(200).json({ message: 'Hero deleted successfully' });
-  });
+        return res.status(200).json({ message: 'Hero deleted successfully' });
+    });
 });
 
 module.exports = router;
